399e4ac458a28454c7113455f470b25c433a4878
Mert <mertala@icloud.com>
2019-10-12 10:43:29 +0300

    Initial Commit

---
+++ TableView-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableView-SwiftUI
+//
+//  Created by Mert Ala on 12.10.2019.
+//  Copyright © 2019 Mert Ala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
